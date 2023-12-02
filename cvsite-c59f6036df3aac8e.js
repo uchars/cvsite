75fa50cc780211e48529b4c40f85bcd37b94e4fb
uchars <uchars@users.noreply.github.com>
2023-12-02 15:10:34 +0000

    deploy: 14fe7a986ae469c56326ad5b4e023b0f6716c662

--- cvsite-c59f6036df3aac8e.js
+++ cvsite-c59f6036df3aac8e.js
@@ -879,7 +879,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('cvsite-d43040f1e81a77d5_bg.wasm', import.meta.url);
+        input = new URL('cvsite-c59f6036df3aac8e_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
